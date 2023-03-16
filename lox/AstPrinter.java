4c80389e627071cd519606c68e95840c8623255d
Zsolt Kébel <kebel.zsolt@gmail.com>
2023-03-16 17:52:43 +0000

    Add Function Calls and a Native Function clock, Chapter 10

--- lox/AstPrinter.java
+++ lox/AstPrinter.java
@@ -2,6 +2,7 @@ package lox;
 
 import lox.Expr.Assign;
 import lox.Expr.Binary;
+import lox.Expr.Call;
 import lox.Expr.Grouping;
 import lox.Expr.Literal;
 import lox.Expr.Logical;
@@ -69,6 +70,11 @@ public class AstPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitLogicalExpr(Logical expr) {
+        throw new UnsupportedOperationException("Unimplemented method 'visitLogicalExpr'");
+    }
+
+    @Override
+    public String visitCallExpr(Call expr) {
         throw new UnsupportedOperationException("Unimplemented method 'visitLogicalExpr'");
     }
 }
--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -1,9 +1,11 @@
 package lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lox.Expr.Assign;
 import lox.Expr.Binary;
+import lox.Expr.Call;
 import lox.Expr.Grouping;
 import lox.Expr.Literal;
 import lox.Expr.Logical;
@@ -14,7 +16,27 @@ import lox.Stmt.While;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
@@ -187,6 +209,29 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         // Unreachable
         return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(expr));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable) callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " +
+                    function.arity() + " arguments but got " +
+                    arguments.size() + ".");
+        }
+
+        return function.call(this, arguments);
     }
 
     @Override
